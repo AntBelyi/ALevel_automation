a21cd388e2009685c8ebda9f78d6a96409028210
Anton_Bilyi <Anton_Bilyi@epam.com>
2024-06-09 23:16:38 +0300

    Add hw directories

---
+++ src/main/java/hw_1/App.java
@@ -0,0 +1,7 @@
+package hw_1;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Home work 1");
+    }
+}
---
+++ src/main/java/hw_2/App.java
@@ -0,0 +1,7 @@
+package hw_2;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Home work 2");
+    }
+}
