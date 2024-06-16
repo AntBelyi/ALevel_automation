a063086dcaabd2abe0dec149a7f837db65fbc01c
Nataliya Kupko <natak8200@gmail.com>
2024-06-16 20:39:59 +0300

    Complete the homework №1

---
+++ src/main/java/hw_1/Task2.java
@@ -0,0 +1,18 @@
+package hw_1;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        System.out.println(a);
+        System.out.println(b);
+
+       int  c = a;
+        a = b;
+        b = c;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
---
+++ src/main/java/hw_1/Task3.java
@@ -0,0 +1,15 @@
+package hw_1;public class Task3 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        a = a + b;
+        b = a -b;
+        a = a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
