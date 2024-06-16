e89ff6e29e27a7b3e89ff9198ae46cd654bb2dfa
AntTurieva <refixings@gmail.com>
2024-06-17 00:01:11 +0300

    made corrections according to the comments

--- src/main/java/hw_1/Task_2.java
+++ src/main/java/hw_1/Task_2.java
@@ -1,11 +1,9 @@
 package hw_1;
 
 public class Task_2 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int a = 1;
         int b = 2;
-
         System.out.println(a);
         System.out.println(b);
 
--- src/main/java/hw_1/Task_3.java
+++ src/main/java/hw_1/Task_3.java
@@ -1,25 +1,15 @@
 package hw_1;
 
 public class Task_3 {
-    public static void main(String[] args)
-    {
-        int a =5;
-        int b =3;
-
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
         System.out.println(a);
         System.out.println(b);
-//if we can use numbers
-    /*    b = b + 2;
-          a = b - 2;
 
-        System.out.println(a);
-        System.out.println(b); */
-//if we can not use numbers
-
-        a = a + b; //5+3=8
-        b = a - b; //8-3=5
-        a = a - b; // 8-5=3
-
+        a = a + b;
+        b = a - b;
+        a = a - b;
         System.out.println(a);
         System.out.println(b);
 
