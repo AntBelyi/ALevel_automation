ad6bf20bc129b8fd49939523c532e4e2b7934f29
Nataliya Kupko <natak8200@gmail.com>
2024-06-18 06:47:51 +0300

    Complete the homework №1

--- src/main/java/hw_1/Task1.java
+++ src/main/java/hw_1/Task1.java
@@ -1,13 +1,10 @@
 package hw_1;
-
 public class Task1 {
     public static void main(String[] args) {
-
-    String name = "Nataliya";
-    int age = 35;
-    float weight = 50.5f;
-    String result = name + " " + age+ " " + weight;
+        String name = "Nataliya";
+        int age = 35;
+        float weight = 50.5f;
+        String result = name + " " + age+ " " + weight;
         System.out.println(result);
-
     }
 }
--- src/main/java/hw_1/Task3.java
+++ src/main/java/hw_1/Task3.java
@@ -3,7 +3,6 @@ public class Task3 {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
-
         System.out.println(a);
         System.out.println(b);
 
