3289d6154b329cb6a5b1a8e5ddd658a2d633299b
Nataliya Kupko <natak8200@gmail.com>
2024-06-25 20:18:18 +0300

    Complete the homework №2

--- src/main/java/hw_2/Task2.java
+++ src/main/java/hw_2/Task2.java
@@ -4,7 +4,7 @@ public class Task2 {
     public static void main(String[] args) {
         int x = 3;
         int y = 4;
-        System.out.println(x + y + " " + x+y);
+        System.out.println(x + y + " " + x + y);
 
     }
 }
