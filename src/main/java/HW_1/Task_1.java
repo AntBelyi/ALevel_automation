96340b5e136be835c939df3253d525f7a7c29424
Kate <ekkrivc@gmail.com>
2024-06-30 20:22:49 +0300

    Fixed bugs# Please enter the commit message for your changes. Lines starting

--- src/main/java/HW_1/Task_1.java
+++ src/main/java/HW_1/Task_1.java
@@ -1,10 +1,10 @@
 package HW_1;
 
 public class Task_1 {
-
+    public static void main(String[] args) {
         String name = "Kate";
         int height = 174;
         float weight = 54.5F;
-        System.out.println(name + ", " + height + " зріст, " + weight + " кг");
+        System.out.println(name + ", " + height + " cm, " + weight + " kg");
     }
 }
