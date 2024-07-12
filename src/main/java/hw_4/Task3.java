5f9367946c57999af3ec94533dcefadbddcb7946
AlinaZvarych <alinazvarich99@gmail.com>
2024-07-12 20:47:41 +0300

    HW 4

--- src/main/java/hw_4/Task3.java
+++ src/main/java/hw_4/Task3.java
@@ -1,16 +1,19 @@
 package hw_4;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
         String text = "Роняє ліс багряний свій убір,\n"
-                    +"Срібне мороз, що в'януло поле,\n"
-                    + "Прогляне день ніби мимоволі\n"
-                    + "І сховається за край окружних гір.";
+                + "Срібне мороз, що в'януло поле,\n"
+                + "Прогляне день ніби мимоволі\n"
+                + "І сховається за край окружних гір.";
         System.out.println("Text:\n" + text);
 
-        String searchTerm = "про";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the substring to search for: ");
+        String searchTerm = scanner.nextLine().toLowerCase();
+
         int count = 0;
         int index = 0;
 
@@ -18,6 +21,7 @@ public class Task3 {
             count++;
             index += searchTerm.length();
         }
-        System.out.println("Number of 'про': " + count);
+
+        System.out.println("Number of '" + searchTerm + "': " + count);
     }
 }
