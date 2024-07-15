4b3da10c79c8d0fecec9750e0c53099c08c350c9
AlinaZvarych <alinazvarich99@gmail.com>
2024-07-15 17:48:10 +0300

    HW 5

--- src/main/java/hw_5/myCity/Building.java
+++ src/main/java/hw_5/myCity/Building.java
@@ -3,18 +3,13 @@ package hw_5.myCity;
 import hw_5.myClass.Person;
 
 public class Building {
-    Person[] residents;
-
-    public Building(int numberOfResidents) {
-        residents = new Person[numberOfResidents];
-    }
     public static void main(String[] args) {
-        Building building = new Building(2);
+        Person[] residents = new Person[2];
 
-        building.residents[0] = new Person("Anna", "Koval", 25);
-        building.residents[1] = new Person("Roman", "Ivanov", 30);
+        residents[0] = new Person("Anna", "Koval", 25);
+        residents[1] = new Person("Roman", "Ivanov", 30);
 
-        for (Person resident : building.residents) {
+        for (Person resident : residents) {
             System.out.println("Name: " + resident.name + ", Surname: " + resident.surname + ", Age: " + resident.age);
         }
     }
