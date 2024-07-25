2eafcd6a7035e76a02df162b580e9d953502b9e9
tanya_bilka <tanyawbw7@gmail.com>
2024-07-25 18:21:44 +0300

    Home Work 13

---
+++ src/main/java/hw_13/CarDTO.java
@@ -0,0 +1,38 @@
+package hw_13;
+
+public class CarDTO {
+    private String model;
+
+    public int getCountOfWheels() {
+        return countOfWheels;
+    }
+
+    public void setCountOfWheels(int countOfWheels) {
+        if (countOfWheels < 4) {
+            throw new InvalidWheelCountException("The wheel count must be more than 3. Attempted to set: " + countOfWheels);
+        }
+
+        this.countOfWheels = countOfWheels;
+    }
+
+    private int countOfWheels;
+    private int maxSpeed;
+    private int years;
+
+    public CarDTO(String model, int countOfWheels, int maxSpeed, int years) {
+        this.model = model;
+        this.countOfWheels = countOfWheels;
+        this.maxSpeed = maxSpeed;
+        this.years = years;
+    }
+
+    @Override
+    public String toString() {
+        return "CarDTO{" +
+                "model='" + model + '\'' +
+                ", countOfWheels=" + countOfWheels +
+                ", maxSpeed=" + maxSpeed +
+                ", years=" + years +
+                '}';
+    }
+}
---
+++ src/main/java/hw_13/InvalidWheelCountException.java
@@ -0,0 +1,7 @@
+package hw_13;
+
+class InvalidWheelCountException extends RuntimeException {
+    public InvalidWheelCountException(String message) {
+        super(message);
+    }
+}
