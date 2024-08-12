42454ae862ea4d9454554d1ba9dcc8e8056bd428
tanya_bilka <tanyawbw7@gmail.com>
2024-08-12 13:15:44 +0300

    Test3

---
+++ src/main/java/hw_16/Test3.java
@@ -0,0 +1,24 @@
+package hw_16;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
+import dev.failsafe.internal.util.Assert;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Test3 {
+    public static void main(String[] args) {
+        final String EMPTY_CART_TEXT = "Кошик порожній";
+
+        open("https://rozetka.com.ua");
+
+        SelenideElement cartItem = $(".header-actions .header-cart__button");
+        cartItem.click();
+
+        SelenideElement elementEmptyCart = $(".cart-dummy__heading").shouldBe(Condition.visible);
+        SelenideElement cartImage = $("img.cart-dummy__illustration").shouldBe(Condition.visible);
+
+        Assert.isTrue(elementEmptyCart.getText().contains(EMPTY_CART_TEXT), "The cart is broken");
+        System.out.println("Test3 is successful");
+    }
+}
