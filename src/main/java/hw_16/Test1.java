0a3c60a1f94bf7e58df46fe775aa746f599abf75
tanya_bilka <tanyawbw7@gmail.com>
2024-08-12 12:53:31 +0300

    Test1

--- src/main/java/hw_16/Test1.java
+++ src/main/java/hw_16/Test1.java
@@ -1,10 +1,9 @@
 package hw_16;
 
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Assertions;
+import dev.failsafe.internal.util.Assert;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Test1 {
 
@@ -13,21 +12,23 @@ public class Test1 {
         final String ERROR_MESSAGE = "Невірний формат номеру посилки. Перевірте вказані символи, а також довжину номеру, яка має бути не більше 13 символів.";
 
         open("https://rozetka.com.ua");
+        sleep(3000);
 
         SelenideElement trackPackageLink = $("a[href*='tracking/']");
         trackPackageLink.click();
+        sleep(3000);
 
         SelenideElement trackPackageInput = $("#searchText");
         SelenideElement trackPackageButton = $(".button.button--medium.button--green");
 
         trackPackageInput.setValue("20450938174406");
         trackPackageButton.click();
+        sleep(3000);
 
         SelenideElement errorMessagePar = $("[class*='validation-message']");
         String errorMessage = errorMessagePar.getText();
 
-        Assertions.assertEquals(ERROR_MESSAGE, errorMessage, "The 'Wrong format package tracking number' is not displayed");
-
+        Assert.isTrue(errorMessage.equals(ERROR_MESSAGE), "The 'Wrong format package tracking number' is not displayed", null);
         System.out.println("Test1 is successful");
 
     }
