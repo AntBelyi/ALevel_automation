8bf0fa50c3e7140e140c0a6ed3da380ca704e028
Anton_Bilyi <Anton_Bilyi@epam.com>
2024-08-19 17:35:53 +0300

    Allure commit

---
+++ src/test/java/experiments/FirstClassTest.java
@@ -0,0 +1,12 @@
+package experiments;
+
+import org.testng.annotations.Test;
+
+public class FirstClassTest {
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, World from first test!");
+    }
+
+}
---
+++ src/test/java/experiments/SecondClassTest.java
@@ -0,0 +1,12 @@
+package experiments;
+
+import org.testng.annotations.Test;
+
+public class SecondClassTest {
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, World from second test!");
+    }
+
+}
