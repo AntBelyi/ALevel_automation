ba536ba5e61158fe14d09e66727fc2722953a337
tanya_bilka <tanyawbw7@gmail.com>
2024-08-22 00:19:59 +0300

    5

--- src/main/java/hw_at_18_rozetka_page_objects/BasePage.java
+++ src/main/java/hw_at_18_rozetka_page_objects/BasePage.java
@@ -21,7 +21,7 @@ public class BasePage {
 
     public BasePage(WebDriver driver){
         this.driver = driver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
     protected void waitForElement(By elementToWait){
