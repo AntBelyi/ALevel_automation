887c9aca9352804ee9768c16873316e8972c8bbb
tanya_bilka <tanyawbw7@gmail.com>
2024-09-13 04:13:16 +0300

    HW 10

--- src/main/java/aqa_hw_18/BasePage.java
+++ src/main/java/aqa_hw_18/BasePage.java
@@ -2,7 +2,6 @@ package aqa_hw_18;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
